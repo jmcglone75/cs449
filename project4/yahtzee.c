7c1a26697b756a9c49dfc7cac0a23a12901cd547
Jake McGlone <jmcglone75@gmail.com>
2016-11-27 15:06:31 -0500

    beggining yahtzee

---
+++ project4/yahtzee.c
@@ -0,0 +1,48 @@
+/*Gerard McGlone
+cs449 Project 4 - /dev/dice
+peoplesoft: 4007177*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+//srand((unsigned int)time(NULL));
+int rolls[5]; //array holding 5 rolls of dice
+int upper[6]; //array holding each of the 6 sections in the upper section
+int lower[7]; //array holding each of the 7 sections in the lower section
+
+int main()
+{
+
+
+
+		
+	return 0;
+}
+
+//returns random number from 1-6
+int roll()
+{
+
+}
+
+//allows player to re-roll any of the five dice and changes the values accordingly
+void re_roll()
+{
+
+}
+
+//player chooses upper/lower section
+//player then picks the sub-section they would like
+//this method will calculate the score appropriatley for whichever subsection is chosen
+void assign_score()
+{
+
+}
+
+//shows upper section, lower section, and upper section bonus
+void show_score()
+{
+
+}
+
