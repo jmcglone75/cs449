bb8f9bac2e0416cd6fccc7f2cb52a6f1cd828adc
Jake McGlone <jmcglone75@gmail.com>
2016-11-27 19:29:21 -0500

    finished upper section

--- project4/yahtzee.c
+++ project4/yahtzee.c
@@ -44,7 +44,12 @@ int main()
 		}
 
 		assign_score();
-		i = 13;
+		for(j = 0; j < 6; j++)
+		{
+			printf("%d  ", upper[j]);
+		}
+//		i = 13;
+		printf("\n");
 	}
 
 		
@@ -65,7 +70,7 @@ void re_roll()
 	char response[11];
 	int reroll[5] = {0, 0, 0, 0, 0};
 
-	printf("\nWhich dice would you like to re-roll(***0 for NO re-roll)?  ");
+	printf("\nWhich dice would you like to re-roll(***0 for NO re-roll)?  \n");
 	fgets(response, sizeof(response), stdin);
 
 	char *token = strtok(response, " ");
@@ -94,14 +99,117 @@ void assign_score()
 	//ask for section
 	int response; 
 	printf("\n\nPlace Dice into:\n1) Upper Section\n2) Lower Section\n");
+	printf("\nSelection? ");
 	scanf("%d", &response);
 	if(response == 1)
 	{
 		//upper
+		int upper_category;
+		int right_category = 0;
+		while(!right_category) //checks to make sure category hasn't been scored in
+		{
+			printf("\n\nPlace Dice into:\n1) Ones\n2) Twos\n3) Threes\n4) Fours\n5) Fives\n6) Sixes\n");
+			printf("\nSelection? ");
+			scanf("%d", &response);
+		
+			upper_category = response - 1;
+			if(upper[upper_category] == 0) //0 means category has not been scored in 
+			{
+				right_category = 1; //break out of loop
+			}
+		}
+
+		if(upper_category == 0) //ones
+		{	
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 1)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+		else if(upper_category == 1) //twos
+		{
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 2)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+		else if(upper_category == 2) //threes
+		{
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 3)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+		else if(upper_category == 3)//fours
+		{
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 4)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+		else if(upper_category == 4)//fives
+		{
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 5)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+		else//sixes
+		{
+			int sum = 0;	
+			int i;
+			for(i = 0; i < 5; i++)
+			{
+				if (rolls[i] == 6)
+					sum = sum + rolls[i];
+			}
+			upper[upper_category] = sum;
+		}
+
+		while(getchar() != '\n'); //flushes stdin - scanf() does not consume new line char which messes up the next call to fgets()
 	}
 	else
 	{
 		//lower
+		int lower_category;
+		int right_category = 0;
+		while(!right_category)
+		{
+			printf("\n\nPlace Dice into:\n1) Three of a Kind\n2) Four of a Kind\n3) Small Straight\n4) Large Straight\n5) Full House\n6) Yahtzee\n7) Chance\n");
+			printf("\nSelection? ");
+			scanf("%d", &response);
+
+			lower_category = response - 1;
+			if(lower[lower_category] == 0)
+			{
+				right_category = 1;
+			}
+		}
+
+		if(lower_category == 0) //three of a kind
+		{
+			
+		}
 	}
 
 	//ask for subsection (make sure it hasn't been filled - use while loop)
@@ -113,4 +221,14 @@ void show_score()
 {
 
 }
+
+
+
+
+
+
+
+
+
+
 
