43c75b6175e9b1aa06521e3dae4f056998ca9653
Jake McGlone <jmcglone75@gmail.com>
2016-12-11 22:25:10 -0500

    done clrf

--- project5/webserver.c
+++ project5/webserver.c
@@ -7,17 +7,18 @@
 #include <pthread.h>
 
 
-void not_found(int client_socket);
-void *accept_request(int client_socket);
+void not_found(void *ptr);
+void *accept_request(void *ptr);
 
 
-void not_found(int client_socket)
+void not_found(void *ptr)
 {
 	return;
 }
 
-void *accept_request(int client_socket)
+void *accept_request(void *ptr)
 {
+	int *connfd = (int *)ptr;
 	char curr = '\0';  //current char being read in - initialized to null
 	int clrf_count = 0; //counter for number of CLRFs
 	int str_count = 0; //position in string -- used to determine if message is > 4KB
@@ -28,16 +29,16 @@ void *accept_request(int client_socket)
 	{
 		if(str_count > sizeof(buffer) + 1)
 		{
-			not_found(client_socket);
+			not_found(connfd);
 			break;
 		}
 
-		int i = recv(client_socket, &curr, 1, 0); //reads one byte from recv
+		int i = recv(*connfd, &curr, 1, 0); //reads one byte from recv
 		if(i > 0)
 		{
 			if(curr == '\r')
 			{
-				i = recv(client_socket, &curr, 1, MSG_PEEK);
+				i = recv(*connfd, &curr, 1, MSG_PEEK);
 				if(i > 0 && curr == '\n')
 					clrf_count++;
 				else
@@ -60,21 +61,29 @@ void *accept_request(int client_socket)
 
 int main()
 {
-	int server_socket, client_socket;
-	struct sockaddr_in addr, client_name;
-	server_socket = socket(PF_INET, SOCK_STREAM, 0);
-	int client_length = sizeof(client_name);
+	int sfd, connfd;
+	struct sockaddr_in addr;
+	sfd = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(50770);
 	addr.sin_addr.s_addr = INADDR_ANY;
 
-	bind(server_socket, (struct sockaddr *)&addr, sizeof(addr));
-	listen(server_socket, 10);
+	bind(sfd, (struct sockaddr *)&addr, sizeof(addr));
+
+	int len = sizeof(addr);
+	unsigned short port;
+	char *ipstring;
+
+/*	port = ntohs(addr.sin_port);
+	ipstring = inet_ntoa(addr.sin_addr);
+
+	printf("%s\n", ipstring);
+	printf("port:%d\n", port);*/
 
 
-	if(server_socket == -1)
+	if(sfd == -1)
 		printf("\nserver could not start\n\n");
 	else
 		printf("\nserver running...\n\n");
@@ -83,15 +92,29 @@ int main()
 
 	while(1)
 	{
-		client_socket = accept(server_socket, (struct sockaddr *)&client_name, &client_length);
-		if(client_socket == -1)
+		listen(sfd, 10);	
+		connfd = accept(sfd, (struct sockaddr *)&addr, &len);
+
+		port = ntohs(addr.sin_port);
+		ipstring = inet_ntoa(addr.sin_addr);
+
+		printf("IP: %s\n", ipstring);
+		printf("Port: %d\n", port);
+		
+		//need to make a struct to pass as argument to accept_request
+		//can't just pass the connfd, also need to pass the client address in 
+		//addition to the connfd.
+		//need to use malloc to assign structs (not just one struct and keep overwriting		//it), because we are going to need that data thoughout the program
+
+		
+		if(connfd == -1)
 			printf("client could not connect\n");
 		else
 			printf("client connected succesfully\n");
 	
 		//create thread that passes accept_request
 
-		if(pthread_create(&newthread, NULL, (void *) accept_request, client_socket) != 0)
+		if(pthread_create(&newthread, NULL, (void *) accept_request, &connfd) != 0)
 		{
 			perror("pthread_create");
 		}
